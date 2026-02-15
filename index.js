876c57aa9cfa6b275062a8812e2380b5335bf37e
Olga Corrales <olgacstrabajando@outlook.com>
2026-02-15 18:27:09 -0500

    Se termino de hacer el put y delete para completar el CRUD de productos con manejo de errores

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');  //Busca la libreria Express que se instalo 
 const errorHandler = require('./middlewares/errorHandler');  //Con esto traemos el traductor de los errores
 const app = express();  //Se guarda en la constante app todas las herramientas para crear rutas, recibir pedidos y enviar respuestas
 
-app.use(express.json()); // Esta linea de código es para entendre datos en formato JSON se usa con el post(crear)
+app.use(express.json()); // Esta linea de código es para entender datos en formato JSON se usa con el post(crear)
 const port = 3000;
 
-//Cree una base de datos temporal o simulada para probar los endpoints
+//Cree una base de datos para probar los endpoints
 let producto = [
     { id: 1, nombre: "Crema corporal", genero: "Mujer", precio: 40000, cantidad: 10 },
     { id: 2, nombre: "Perfume Dorsay", genero: "Hombre", precio: 65000, cantidad: 5 },
@@ -62,6 +62,52 @@ app.post('/productos', (req, res, next) => {
 
     producto.push(nuevoProducto);  //Aquí lo empujamos o guardamos en la lista producto
     res.status(201).json(nuevoProducto);   //Asi se responde con el producto creado ye su codigo de esito
+});
+
+//Esta ruta es para actualizar un producto existente
+app.put('/productos/:id', (req, res, next) => {
+    const idUrl = req.params.id;
+
+    //Aqui busca la posición del producto y lo combierte en String para que sea igual a de la Url
+    const indice = producto.findIndex(p => p.id.toString() === idUrl);
+
+    if (indice === -1) {
+        const error = new Error("No existe el producto");
+        error.status = 404;
+        return next(error);
+    }
+
+    //Mezclamos el producto antiguo con el actualizado
+    producto[indice] = {
+        ...producto[indice],
+        ...req.body
+    };
+
+    res.json({
+        mensaje: "Producto Actualizado con Exito",
+        producto: producto[indice]
+    })
+});
+
+//Aqui la ruta para eliminar un producto
+app.delete('/productos/:id', (req, res, next) => {
+    const idUrl = req.params.id; //con esta linea se extrae el id que se escribio en la url y se guarda
+
+    //aqui se esta buscando el producto con indice
+    const indice = producto.findIndex(p => p.id.toString() === idUrl);
+
+    if (indice === -1) {
+        const  error =  new Error("No se encontró el producto");
+        error.status = 404;
+        return next(error);
+    } 
+    
+    //Aqui ya eliminamos el producto 
+    const eliminado = producto.splice(indice, 1);
+    res.json({
+        mensaje: "El producto fue eliminado con exito",
+        productoBorrado: eliminado[0]
+    });
 });
 
 app.use(errorHandler);  // con este usamos el archivo externo de manejo de errores para atrapar el error
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 // Este archivo es solamente para manejar errores
-const errorHandler = (err, req, re, next) => {
+const errorHandler = (err, req, res, next) => {
     console.error(err.stack);  //Este código muestra el error en la terminal
     const status = err.status || 500;
     const mensaje = err. message || 'Error interno del servidor';
